66ec9ad9081599c0246d6eeecdf20d12fce3c63c
Tuyen Dang <tuyendq@gmail.com>
2021-06-15 23:08:36 +0700

    Initial commit

---
+++ src/main/java/net/practicehabits/springjpah2/SpringJpaH2Application.java
@@ -0,0 +1,13 @@
+package net.practicehabits.springjpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/practicehabits/springjpah2/SpringJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.practicehabits.springjpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
